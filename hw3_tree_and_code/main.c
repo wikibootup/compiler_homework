c37dac7dfb63d21e64c5234d3c19891cf95ef35e
wikibootup <wikibootup@gmail.com>
2016-11-02 20:00:48 +0900

    init main, type

---
+++ hw3_tree_and_code/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "type.h"
+extern FILE *yyin;
+extern int syntax_err;
+extern A_NODE *root;
+FILE *fout;
+void initialize();
+void print_ast();
+extern FILE *yyin;
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("source file not given\n");
+        exit(1);
+    }
+    
+    if ((yyin = fopen(argv[argc-1],"r")) == NULL) {
+        printf("can not open input file: %s\n",argv[argc-1]);
+        exit(1);
+    }
+    
+    printf("\nstart syntax analysis\n"); initialize();
+    
+    yyparse();
+    
+    if (syntax_err)
+        exit(1);
+    
+    print_ast(root);
+    exit(0);
+
+    return 0;
+}
